e3592bc65ba2bc4cb8f2c1229b5e65fb6df7a98e
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2025-09-25 21:15:20 -0300

    Vista Razor y Funcionalidad en Controller de Crear

--- discos-console-db/discos-mvc/Controllers/DiscosController.cs
+++ discos-console-db/discos-mvc/Controllers/DiscosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using dominio;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using negocio;
 
 namespace discos_mvc.Controllers
@@ -23,16 +25,29 @@ namespace discos_mvc.Controllers
         // GET: DiscosController/Create
         public ActionResult Create()
         {
+            EstiloNegocio negocio = new EstiloNegocio();
+            TipoEdicionNegocio tipoEdicionNegocio = new TipoEdicionNegocio();
+            ViewBag.Estilos = new SelectList(negocio.listar(), "Id", "Descripcion");
+            ViewBag.TiposEdicion = new SelectList(tipoEdicionNegocio.listar(), "Id", "Descripcion");
             return View();
         }
 
         // POST: DiscosController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Disco disco)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return View(disco);
+                }
+
+                DiscoNegocio negocio = new DiscoNegocio();
+                disco.Estilo = new Estilo { Id = 1 }; // Esto debería venir de la base de datos
+                disco.TipoEdicion = new TipoEdicion { Id = 1 }; // Esto debería venir de la base de datos
+                negocio.agregar(disco);
                 return RedirectToAction(nameof(Index));
             }
             catch
--- discos-console-db/dominio/Disco.cs
+++ discos-console-db/dominio/Disco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace dominio
         public int Id { get; set; }
         public string Titulo { get; set; }
         public DateTime FechaLanzamiento { get; set; }
+
+        [Range (1, int.MaxValue, ErrorMessage = "La cantidad de canciones debe ser al menos 1.")]
         public int CantidadCanciones { get; set; }
         public string UrlTapa { get; set; }
         public Estilo Estilo { get; set; }
