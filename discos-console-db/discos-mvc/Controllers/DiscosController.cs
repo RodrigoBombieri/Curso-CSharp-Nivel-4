3d8378ab9d1533adfeb93766ba6d17cd73a7554c
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2025-09-27 11:21:44 -0300

    ABML Discos OK, comenzando AMBL Estilos

--- discos-console-db/discos-mvc/Controllers/DiscosController.cs
+++ discos-console-db/discos-mvc/Controllers/DiscosController.cs
@@ -9,17 +9,23 @@ namespace discos_mvc.Controllers
     public class DiscosController : Controller
     {
         // GET: DiscosController
-        public ActionResult Index()
+        public ActionResult Index(string filtro)
         {
             DiscoNegocio negocio = new DiscoNegocio();
             var discos = negocio.listar();
+            if (!string.IsNullOrEmpty(filtro))
+            {
+                discos = discos.FindAll(d => d.Titulo.Contains(filtro));
+            }
             return View(discos);
         }
 
         // GET: DiscosController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            DiscoNegocio negocio = new DiscoNegocio();
+            var disco = negocio.listar().FirstOrDefault(d => d.Id == id);
+            return View(disco);
         }
 
         // GET: DiscosController/Create
@@ -59,16 +65,28 @@ namespace discos_mvc.Controllers
         // GET: DiscosController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            DiscoNegocio negocio = new DiscoNegocio();
+            TipoEdicionNegocio tipoEdicionNegocio = new TipoEdicionNegocio();
+            EstiloNegocio estiloNegocio = new EstiloNegocio();
+            var disco = negocio.listar().FirstOrDefault(d => d.Id == id);
+
+            // Cargar los SelectList para Estilos y Tipos de Edición
+            var listaEstilos = estiloNegocio.listar();
+            var listaTiposEdicion = tipoEdicionNegocio.listar();
+            ViewBag.Estilos = new SelectList(listaEstilos, "Id", "Descripcion", disco.Estilo.Id);
+            ViewBag.TipoEdicion = new SelectList(listaTiposEdicion, "Id", "Descripcion", disco.TipoEdicion.Id);
+            return View(disco);
         }
 
         // POST: DiscosController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Disco disco)
         {
             try
             {
+                DiscoNegocio negocio = new DiscoNegocio();
+                negocio.modificar(disco);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -80,16 +98,20 @@ namespace discos_mvc.Controllers
         // GET: DiscosController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            DiscoNegocio negocio = new DiscoNegocio();
+            var disco = negocio.listar().FirstOrDefault(d => d.Id == id);
+            return View(disco);
         }
 
         // POST: DiscosController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Disco disco)
         {
             try
             {
+                DiscoNegocio negocio = new DiscoNegocio();
+                negocio.eliminar(id); // Eliminación física
                 return RedirectToAction(nameof(Index));
             }
             catch
---
+++ discos-console-db/discos-mvc/Controllers/EstilosController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace discos_mvc.Controllers
+{
+    public class EstilosController : Controller
+    {
+        // GET: EstilosController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EstilosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EstilosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EstilosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EstilosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EstilosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EstilosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EstilosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
