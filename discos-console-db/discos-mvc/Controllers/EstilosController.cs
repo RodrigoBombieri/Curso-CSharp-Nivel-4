3d7154c6c58d7702fbde77c7da07a5c63c4a9c26
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2025-09-27 16:36:37 -0300

    ABML Estilos OK

--- discos-console-db/discos-mvc/Controllers/EstilosController.cs
+++ discos-console-db/discos-mvc/Controllers/EstilosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using dominio;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using negocio;
 
 namespace discos_mvc.Controllers
 {
@@ -8,7 +10,9 @@ namespace discos_mvc.Controllers
         // GET: EstilosController
         public ActionResult Index()
         {
-            return View();
+            EstiloNegocio negocio = new EstiloNegocio();
+            var estilos = negocio.listar();
+            return View(estilos);
         }
 
         // GET: EstilosController/Details/5
@@ -26,10 +30,12 @@ namespace discos_mvc.Controllers
         // POST: EstilosController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Estilo estilo)
         {
             try
             {
+                EstiloNegocio negocio = new EstiloNegocio();
+                negocio.agregar(estilo);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -41,16 +47,20 @@ namespace discos_mvc.Controllers
         // GET: EstilosController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            EstiloNegocio negocio = new EstiloNegocio();
+            Estilo estilo = negocio.listar().FirstOrDefault(e => e.Id == id);
+            return View(estilo);
         }
 
         // POST: EstilosController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Estilo estilo)
         {
             try
             {
+                EstiloNegocio negocio = new EstiloNegocio();
+                negocio.modificar(estilo);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -62,7 +72,9 @@ namespace discos_mvc.Controllers
         // GET: EstilosController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            EstiloNegocio negocio = new EstiloNegocio();
+            Estilo estilo = negocio.listar().FirstOrDefault(e => e.Id == id);
+            return View(estilo);
         }
 
         // POST: EstilosController/Delete/5
@@ -72,6 +84,8 @@ namespace discos_mvc.Controllers
         {
             try
             {
+                EstiloNegocio negocio = new EstiloNegocio();
+                negocio.eliminar(id);
                 return RedirectToAction(nameof(Index));
             }
             catch
--- discos-console-db/negocio/EstiloNegocio.cs
+++ discos-console-db/negocio/EstiloNegocio.cs
@@ -40,5 +40,63 @@ namespace negocio
                 datos.cerrarConexion();
             }
         }
+
+        public void agregar(Estilo nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Insert into ESTILOS (Descripcion) values (@Descripcion)");
+                datos.setearParametro("@Descripcion", nuevo.Descripcion);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Estilo estilo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Update ESTILOS set Descripcion = @Descripcion where Id = @Id");
+                datos.setearParametro("@Descripcion", estilo.Descripcion);
+                datos.setearParametro("@Id", estilo.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Delete from ESTILOS where Id = @Id");
+                datos.setearParametro("@Id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
