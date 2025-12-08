a6c945e63d2650409799710252b6a11d6f4d62b6
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2025-12-08 16:50:18 -0300

    Home y Detalle

--- ejemplo-galeria-arte/galeria-arte-mvc/Controllers/HomeController.cs
+++ ejemplo-galeria-arte/galeria-arte-mvc/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 using System.Diagnostics;
+using galeria_arte_mvc.Data;
 using galeria_arte_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace galeria_arte_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly GaleriaDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, GaleriaDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var obras = await _context.Obras
+                .Take(6)
+                .Include(o => o.Artista)
+                .ToListAsync();
+            return View(obras);
         }
 
         public IActionResult Privacy()
---
+++ ejemplo-galeria-arte/galeria-arte-mvc/Controllers/ObraController.cs
@@ -0,0 +1,31 @@
+﻿using galeria_arte_mvc.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace galeria_arte_mvc.Controllers
+{
+    public class ObraController : Controller
+    {
+        private readonly GaleriaDbContext _context;
+        public ObraController(GaleriaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> DetalleObra(string id)
+        {
+            var obra = await _context.Obras.Include(o => o.Artista)
+                .FirstOrDefaultAsync(o => o.Id.ToString() == id);
+            if (obra == null)
+                {
+                return NotFound();
+            }
+            return View(obra);
+        }
+    }
+}
