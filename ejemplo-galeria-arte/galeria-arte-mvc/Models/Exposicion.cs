f197165454d963754e7238604629497ff18d8bbc
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2025-12-08 17:56:24 -0300

    Agregar obras a exposición

--- ejemplo-galeria-arte/galeria-arte-mvc/Models/Exposicion.cs
+++ ejemplo-galeria-arte/galeria-arte-mvc/Models/Exposicion.cs
@@ -1,10 +1,14 @@
-﻿namespace galeria_arte_mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace galeria_arte_mvc.Models
 {
     public class Exposicion
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        [Display(Name = "Fecha de Inicio")]
         public DateTime FechaInicio { get; set; }
+        [Display(Name = "Fecha de Fin")]
         public DateTime FechaFin { get; set; }
         public List<Obra>? ObrasExpuestas { get; set; }
     }
