b7876a6854af0f75546580f040ce698ac6d5da91
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2026-01-08 18:14:44 -0300

    Creando el Home

--- rodri-movie-mvc-EJEMPLO/rodri-movie-mvc/Controllers/HomeController.cs
+++ rodri-movie-mvc-EJEMPLO/rodri-movie-mvc/Controllers/HomeController.cs
@@ -1,21 +1,64 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using rodri_movie_mvc.Data;
 using rodri_movie_mvc.Models;
+using System.Diagnostics;
 
 namespace rodri_movie_mvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MovieDbContext _context;
+        private const int PageSize = 8;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MovieDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int pagina = 1, string txtBusqueda = "", int generoId = 0)
         {
-            return View();
+            if (pagina < 1) pagina = 1;
+
+            var consulta = _context.Peliculas.AsQueryable();
+            if (!string.IsNullOrEmpty(txtBusqueda))
+            {
+                consulta = consulta.Where(p => p.Titulo.Contains(txtBusqueda));
+            }
+
+            if (generoId > 0)
+            {
+                consulta = consulta.Where(p => p.GeneroId == generoId);
+            }
+
+            var totalPeliculas = await consulta.CountAsync();
+            var totalPaginas = (int)Math.Ceiling(totalPeliculas / (double)PageSize);
+
+            if (pagina > totalPaginas && totalPaginas > 0) pagina = totalPaginas;
+
+            var peliculas = await consulta
+                .Skip((pagina - 1) * PageSize)
+                .Take(PageSize)
+                .ToListAsync();
+
+            ViewBag.PaginaActual = pagina;
+            ViewBag.TotalPaginas = totalPaginas;
+            ViewBag.TotalPeliculas = totalPeliculas;
+            ViewBag.TxtBusqueda = txtBusqueda;
+
+            var generos = await _context.Generos.OrderBy(g => g.Descripcion).ToListAsync();
+            generos.Insert(0, new Genero { Id = 0, Descripcion = "Género" });
+            ViewBag.GeneroId = new SelectList(
+                generos,
+                "Id",
+                "Descripcion",
+                generoId
+            );
+
+            return View(peliculas);
         }
 
         public IActionResult Privacy()
