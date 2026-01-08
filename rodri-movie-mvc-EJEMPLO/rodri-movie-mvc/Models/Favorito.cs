08efb8c3c02c46ca8a25c90feada50f2896e2022
Rodrigo Bombieri <rodrigosbombieri@gmail.com>
2026-01-07 21:19:32 -0300

    Migracion inicial y creacion de la DB

--- rodri-movie-mvc-EJEMPLO/rodri-movie-mvc/Models/Favorito.cs
+++ rodri-movie-mvc-EJEMPLO/rodri-movie-mvc/Models/Favorito.cs
@@ -1,11 +1,15 @@
-﻿namespace rodri_movie_mvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rodri_movie_mvc.Models
 {
     public class Favorito
     {
+        public int Id { get; set; }
         public int UsuarioId { get; set; }
         public Usuario? Usuario { get; set; }
         public int PeliculaId { get; set; }
         public Pelicula? Pelicula { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Fecha { get; set; }
 }
 }
